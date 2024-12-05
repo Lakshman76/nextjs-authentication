3a0ed3a6b3ed67f0071f8d8e77f72d7f5287606c
Lakshman76 <lakshmankumar2603@gmail.com>
2024-12-05 22:13:11 +0530

    implement middleware

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+    const path = request.nextUrl.pathname;
+    const isPublicPath = path === '/login' || path === '/signup';
+    const token = request.cookies.get('token')?.value || '';
+
+    if (isPublicPath && token) {
+        return NextResponse.redirect(new URL('/', request.url))
+    }
+
+    if (!isPublicPath && !token) {
+        return NextResponse.redirect(new URL('/login', request.url))
+    }
+}
+
+export const config = {
+    matcher: [
+        '/',
+        '/profile/:path*',
+        '/login',
+        '/signup'
+    ]
+}
