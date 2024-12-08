3c1bf20097ed8f9b27dfb14789db8bc4801aa228
Lakshman76 <lakshmankumar2603@gmail.com>
2024-12-08 22:23:17 +0530

    add verifyemail route

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
     const path = request.nextUrl.pathname;
-    const isPublicPath = path === '/login' || path === '/signup';
+    const isPublicPath = path === '/login' || path === '/signup' || path === '/verifyemail';
     const token = request.cookies.get('token')?.value || '';
 
     if (isPublicPath && token) {
@@ -19,6 +19,7 @@ export const config = {
         '/',
         '/profile/:path*',
         '/login',
-        '/signup'
+        '/signup',
+        '/verifyemail'
     ]
 }
