7d5831a427db5cc92e9128434bcc0a9270f40aed
Lakshman76 <lakshmankumar2603@gmail.com>
2024-12-10 22:26:50 +0530

    helper for reset password

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -29,7 +29,7 @@ export const sendEmail = async ({ email, emailType, userId }: any) => {
             from: "lakshmankumar2603@gmail.com",
             to: email,
             subject: emailType === "VERIFY" ? "Verify your email" : "Reset your password",
-            html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}</p>`
+            html: `<p>Click <a href="${process.env.DOMAIN}/${emailType === "VERIFY" ? "verifyemail" : "resetpassword"}?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}</p>`
 
         }
         const mailResponse = await transport.sendMail(mailOptions);
