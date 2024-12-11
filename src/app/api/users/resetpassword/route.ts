9cde9e7b76e673eb0a90c1a278af5d53061dd45e
Lakshman76 <lakshmankumar2603@gmail.com>
2024-12-11 20:54:51 +0530

    reset password route

---
+++ src/app/api/users/resetpassword/route.ts
@@ -0,0 +1,37 @@
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import { connect } from "@/config/dbConfig";
+import bcryptjs from "bcryptjs";
+
+connect();
+
+export async function POST(request: NextRequest) {
+    try {
+        const reqBody = await request.json();
+        const { newPassword, token } = reqBody;
+
+        const user = await User.findOne({ forgotPasswordToken: token, forgotPasswordTokenExpiry: { $gt: Date.now() } });
+
+        if (!user) {
+            return NextResponse.json({ error: "Invalid or expired token" }, { status: 400 });
+        }
+
+        // Hash password
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPassword = await bcryptjs.hash(newPassword, salt);
+
+        user.password = hashedPassword;
+        user.verifyToken = undefined;
+        user.verifyTokenExpiry = undefined;
+
+        await user.save();
+
+        return NextResponse.json({
+            message: "Password reset successfully",
+            success: true,
+        })
+
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+}
